4de4f53d4383555fe628ac3c6447d1f30834e9bd
editmond <42686022+editmond@users.noreply.github.com>
2025-01-16 11:51:23 +1100

    copy and paste pieces

--- src/chess_logic/game_control.py
+++ src/chess_logic/game_control.py
@@ -1,5 +1,9 @@
 def game_start():
     #initialise board
+    for j in range(8):
+        row = []
+        for i range(8):
+            row.append([])
     #
     return
 
--- src/chess_logic/pieces.py
+++ src/chess_logic/pieces.py
@@ -9,7 +9,66 @@ class Rook(Piece):
         super().__init__(position, target, isWhite)
 
     def __str__(self):
-        return f"A {self.isWhite} at {self.position}"
+        colour = ("Black", "White")[self.isWhite]
+        return f"{colour} at {self.position}"
 
     def moveCheck(self):
-        print(f"check {self} can move to {self.target}")
+        print(f"Check if a {self} can move to {self.target}")
+
+class Knight(Piece):
+    def __init__(self, position, target, isWhite):
+        super().__init__(position, target, isWhite)
+
+    def __str__(self):
+        colour = ("Black", "White")[self.isWhite]
+        return f"{colour} at {self.position}"
+
+    def moveCheck(self):
+        print(f"Check if a {self} can move to {self.target}")
+
+class Bishop(Piece):
+    def __init__(self, position, target, isWhite):
+        super().__init__(position, target, isWhite)
+
+    def __str__(self):
+        colour = ("Black", "White")[self.isWhite]
+        return f"{colour} at {self.position}"
+
+    def moveCheck(self):
+        print(f"Check if a {self} can move to {self.target}")
+
+class Queen(Piece):
+    def __init__(self, position, target, isWhite):
+        super().__init__(position, target, isWhite)
+
+    def __str__(self):
+        colour = ("Black", "White")[self.isWhite]
+        return f"{colour} at {self.position}"
+
+    def moveCheck(self):
+        print(f"Check if a {self} can move to {self.target}")
+
+class King(Piece):
+    def __init__(self, position, target, isWhite):
+        super().__init__(position, target, isWhite)
+
+    def __str__(self):
+        colour = ("Black", "White")[self.isWhite]
+        return f"{colour} at {self.position}"
+
+    def moveCheck(self):
+        print(f"Check if a {self} can move to {self.target}")
+
+class Pawn(Piece):
+    def __init__(self, position, target, isWhite):
+        super().__init__(position, target, isWhite)
+
+    def __str__(self):
+        colour = ("Black", "White")[self.isWhite]
+        return f"{colour} at {self.position}"
+
+    def moveCheck(self):
+        print(f"Check if a {self} can move to {self.target}")
+
+
+
